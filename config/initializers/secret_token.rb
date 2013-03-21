5a478b3cc927555897188c18e2487ab5eebc4424
Leif Ringstad <leifcr@gmail.com>
2013-03-21 14:09:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beartestraketask::Application.config.secret_token = 'fe9a1cd9849daaa599d4b0dabd343c93ddd7fc4fbb6d5d5636484a12a892cc011722dcf9ce8d59da2a18348844e909c5c091a22bc47e4b0863607c0758cd656b'
